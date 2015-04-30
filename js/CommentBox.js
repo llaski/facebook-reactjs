10bb2cf0cf6dc16614925b8cc262a5bd471096f9
Larry Laski <larry.laski@gmail.com>
2015-04-30 10:00:29 -0400

    getting updates from server

--- js/CommentBox.js
+++ js/CommentBox.js
@@ -1,15 +1,40 @@
 var CommentBox = React.createClass({
 
-	render: function() {
+	getInitialState: function getInitialState()
+	{
+		return { data: [] };
+	},
+
+	componentDidMount: function componentDidMount()
+	{
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
+	},
+
+	render: function render() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
+	},
+
+	loadCommentsFromServer: function loadCommentsFromServer()
+	{
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json'
+		}).done(function(data)
+		{
+			this.setState({data: data});
+		}.bind(this)).fail(function(jqXHR, textStatus, errorThrown)
+		{
+			console.error(this.props.url, textStatus, errorThrown.toString());
+		}.bind(this));
 	}
 
 });
 
-React.render(<CommentBox data={App.data} url="comments.json" />, document.getElementById('content'));
+React.render(<CommentBox url="comments.json" pollInterval={2000} />, document.getElementById('content'));
